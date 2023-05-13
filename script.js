84f247e636fdcdbb1d57ab546d2ad00268508fb2
saurabh singh <86177535+saurabhsngh5484@users.noreply.github.com>
2023-05-13 21:55:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-// complete this js code
-function Person(name, age) {}
+function Person(name, age) {
+	this.name=name;
+	this.age=age;
+}
+Person.prototype.greet=function () {
+	console.log('Hello, my name is Alice and I am 25 years old.')
+}
+function Employee(name, age, jobTitle) {
+	this.name=name;
+	this.age=age;
+	this.jobTitle=jobTitle;
+}
 
-function Employee(name, age, jobTitle) {}
+Employee.prototype = Object.create(Person.prototype);
 
+
+Employee.prototype.jobGreet=function()
+	{
+		console.log( 'Hello, my name is Bob, I am 30 years old, and my job title is Manager.')
+	}
 // Do not change code below this line
 window.Person = Person;
 window.Employee = Employee;
